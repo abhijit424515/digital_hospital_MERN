c113cc45c8207085df6a5da5c97677552078934d
Anand Narasimhan <anand.nara123@gmail.com>
2022-11-03 00:59:47 +0530

    Added style(button) and trying Bootstrap

--- new/src/App.jsx
+++ new/src/App.jsx
@@ -1,11 +1,13 @@
 import DisplayArea from "./components/DisplayArea"
 import Button from "./components/Button"
+import Navbar from "./components/Navbar"
 
 export default function App() {
    return (
    <div className="h-auto">
-      <DisplayArea cl={"bg-blue-300 h-1/2 p-[1rem]"} />
-      <Button text={'Button'}/>
+      <Navbar />
+      <Button text={'Button'} bgColor={'bg-yellow-500'} hoverColor={'bg-purple-500'} />
+      {/* <DisplayArea cl={"bg-blue-300 h-1/2 p-[1rem]"} /> */}
    </div>
    )
 }
