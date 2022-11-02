875b6f9be20caa5f89a397036a1692ef37b33512
PrrsnCrVS4sr <96426776+PrrsnCrVS4sr@users.noreply.github.com>
2022-11-03 00:03:23 +0530

    button added

--- new/src/App.jsx
+++ new/src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar"
+import Button from "./components/Button"
 
 export default function App() {
    return (
    <>
       <Navbar />
+      <Button text={'Button'}/>
    </>
    )
 }
