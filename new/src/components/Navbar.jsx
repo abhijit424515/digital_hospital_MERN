e8fd93f6b8461039050c3d9b3d02fc97cb51993f
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2022-11-15 16:37:37 +0530

    NavBar responisive and  icons done

--- new/src/components/Navbar.jsx
+++ new/src/components/Navbar.jsx
@@ -1,26 +1,37 @@
 import React from 'react'
 import Button from './Button'
+import "../styles/NavBar.css"
 
-import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { faBookMedical, faFile, faFileMedical, faHouse, faKitMedical, faMoneyBill, faTablet, faUserDoctor } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 const nav_headers = [
-    [faHouse,"Home"], 
-    [faHouse,"Medical Book"], 
-    [faHouse,"Doctor's Visit"], 
-    [faHouse,"Pharmacy"], 
-    [faHouse,"Lab Order"], 
-    [faHouse,"Reports"], 
-    [faHouse,"Doctor's Availability"], 
-    [faHouse,"Doctor's Appoinment"], 
-    [faHouse,"Reimburse"] 
+    [faHouse, "Home"],
+    [faBookMedical, "Medical Book"],
+    [faUserDoctor, "Doctor's Visit"],
+    [faKitMedical, "Pharmacy"],
+    [faFileMedical, "Lab Order"],
+    [faFile, "Reports"],
+    [faUserDoctor, "Doctor's Availability"],
+    [faUserDoctor, "Doctor's Appoinment"],
+    [faMoneyBill, "Reimburse"]
 ]
 
-export default function Navbar(props) {    
+export default function Navbar(props) {
     return (
-        <nav className="navbar navbar-expand-lg bg-light">
-            <div className="container-fluid">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navcontainer collapse navbar-collapse justify-content-around" id="navbarNavAltMarkup">
                 {nav_headers.map((item) => (
-                    <Button key={item} fai={item[0]} tw={"h-[4rem] m-1 bg-sky-500 hover:bg-sky-700"} text={item[1]} click={() => { props.changeState(item[1])}} />
+                    <a key={item} class="navigationitem nav-item nav-link text-light text-center fs-2 hover:text-blue-600" onClick={() => { document.getElementsByClassName("navbar-toggler")[0].click(); props.changeState(item[1]) }} >
+                        <span>
+                            <FontAwesomeIcon icon={item[0]} />
+                            <span class="ms-2">{item[1]}</span>
+                        </span>
+                    </a>
                 ))}
             </div>
         </nav>
